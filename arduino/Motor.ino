7a601b415090b84b6f4c1db041ff90991f3fcbf6
Louis Lim <louis_lim97@outlook.com>
2018-04-03 01:57:44 -0500

    Update
    Main Class reorganized

--- arduino/Motor.ino
+++ arduino/Motor.ino
@@ -1,62 +1,85 @@
-//Arduino PID library: https://playground.arduino.cc/Code/PIDLibrary
-#include <PID_v1.h>
-#include <Sabertooth.h>
-Sabertooth ST(128);
-int A = 6;
-int B = 5;
-int X = 7;
-int power;
-
-double Setpoint, Input, Output; //Define Variables we'll be connecting to
-double kp = 0.01, ki = 0.5, kd = 0;
-//0.01, 0.2, 0.009 - Optimum 12/21 3.18pm - when sample time is 100ms
-//0.01, 0.5, 0.001 - Optimum 12/21 3.42pm - when sample time is 100ms
-//When kd is 0 , diff by 2 value max
-//Specify the links and intial tuning parameters
-
-PID myPID(&Input, &Output, &Setpoint, kp, ki, kd, DIRECT);
-int tolerance = 5; //threshold for change kp
-
-void motorSetup() {
-	pinMode(B, INPUT_PULLUP);
-	pinMode(A, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(A), count, RISING);
-	
-	SabertoothTXPinSerial.begin(9600); // 9600 is the default baud rate for Sabertooth packet serial.
-	ST.autobaud(); // Send the autobaud command to the Sabertooth controller(s).
-	ST.motor(1, 0); //Stop motor when reset
-
-	//Input = analogRead(PIN_POT); //initialize the variables we're linked to
-	Setpoint = 100; // initial set point
-	myPID.SetOutputLimits(-127, 127); // power limits of sabertooth
-	myPID.SetMode(AUTOMATIC); //turn the PID on
-	myPID.SetSampleTime(100); // 10ms works fine for Motor
-	ST.address();
-	ST.setTimeout(0);
+#include "RMCKangaroo.h"
+#include <Kangaroo.h>
+RMCKangaroo motorK(Serial3);
+void setup() {
+	motorK.begin();
+	Serial1.begin(115200);
+	Serial.begin(115200);
+	Serial.println("Motor control started!");
 }
 
-void motorLoop() {
-	myPID.SetTunings(kp, ki, kd);
-	if (myPID.Compute() == true) {
+void loop() {
+	motorK.loop();
+	serialEvent();
+}
 
-		if (Setpoint == 0 && Input == 0)
-			 power = 0;
-		else
-			 power = Output;
-		
-		ST.motor(1, power);
-		Input = 0;
+void serialEvent() {
+	while (Serial1.available() > 4) {
+		char message[5];
+		Serial1.readBytes(message, 5);
+		if (checkSum(message, 5))
+		{
+			char command = message[0];
+			char device = message[1];
+			char value1 = message[2];
+			char value2 = message[3];
+			//Serial.println("Passed checksum: " + String(command) + " " + String(device) + " " + String(value1) + " " + String(value2));
+			switch (command)
+			{
+			case 1:
+				switch (device)
+				{
+				case 0:
+					//Emergency Stop. Should stop all motors
+					motorK.motors->drive(0, 0);
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6: //channel 1 and 2 together
+					break;
+				case 8:
+					break;
+				case 10:
+					motorK.motors->drive((signed char)value1, (signed char)value2);
+					break;
+				case 11:
+					motorK.motors->tankDrive((signed char)value1, (signed char)value2);
+					break;
+				default:
+					Serial.println("unhanddle command: " + String(command) + " " + String(device) + " " + String(value1) + " " + String(value2));
+					break;
+				}
+			}
+		}
 	}
-
-//	if (abs(Input - Setpoint) < tolerance) kp = 0.1;
-//	else kp = 3;
-	//delay(300);
-	//{@Plot.Pos.currentSpeed.Green Input}, {@Plot.Pos.targetSpeed.Blue Setpoint}, {@Plot.Power.Power.Red Output}, {@Plot.Constant.kp.Blue kp}, {@Plot.Constant.ki.Red ki}, {@Plot.Constant.kd.Green kd},kp is {kp=?}, ki is {ki=?}, kd is {kd=?}, Power is {Output=?}, Setpoint is {Setpoint =?}
 }
-
-void count() {
-	if (digitalRead(B) == LOW)
-		Input++;
+bool checkSum(char arrayNum[], int len) {
+	char sum = 0;
+	for (int i = 0; i < len; i++)
+	{
+		sum += arrayNum[i];
+	}
+	if (sum == 0)
+	{
+		return true;
+	}
 	else
-		Input--;
+	{
+		Serial.print("Checksum fails.");
+		for (int i = 0; i < len; i++)
+		{
+			Serial.print(arrayNum[i]);
+			Serial.print(" ");
+		}
+		Serial.println();
+		return false;
+	}
 }
